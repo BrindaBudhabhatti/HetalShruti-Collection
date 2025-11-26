1a026a5dbd1f1f77bc9ffc6013e6bfd979356ef2
Brinda Budhabhatti <brinda.budhabhatti@gmail.com>
2025-11-26 12:08:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9470226726-762f6",
+  "appId": "1:505542357948:web:4ddf2a3bd481c82c9c71b9",
+  "apiKey": "AIzaSyBsTGx7cPzYQjayUUMvTz92F_JlOx21VtM",
+  "authDomain": "studio-9470226726-762f6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "505542357948"
+};
